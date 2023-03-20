1c8ead72f9be501beeb8e00126d59ab649059ab9
TaThuHa <thuhak2tltnpt@gmail.com>
2023-03-20 16:29:47 +0700

    up

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,39 @@
 import java.awt.*;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
-    public static void Menu() {
-        System.out.println("****************JAVA-HACKATHON-05-ADVANCE-1-MENU***************\n" +
-                "1. Nhập URL muốn truy cập\n" +
-                "2. Quay lại\n" +
-                "3. Thoát");
-    }
+
 
     public static void main(String[] args) {
-        Menu();
-        Scanner input = new Scanner(System.in);
-        System.out.println("chọn: ");
-        int choice = Integer.parseInt(input.nextLine());
-        while (true){
-        switch (choice){
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                System.exit(0);
-                break;
-            default:
-                System.out.println("chọn lại");
-        }
-    }
+        Stack<String> stack = new Stack<>();
+
+
+        do {
+
+            System.out.println("****************JAVA-HACKATHON-05-ADVANCE-1-MENU***************\n" +
+                    "1. Nhập URL muốn truy cập\n" +
+                    "2. Quay lại\n" +
+                    "3. Thoát");
+            System.out.println("chọn: ");
+            Scanner input = new Scanner(System.in);
+            int choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Nhập URL: ");
+                    input.nextLine();
+                    String url = input.nextLine();
+                    stack.push(url);
+                    break;
+                case 2:
+                    stack.peek();
+                    break;
+                case 3:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("chọn lại");
+            }
+        }while (true);
     }
 }
